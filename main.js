9e09b22a141716237bf03ed76e1175b6381d8c91
PavelMerkushevich <11mpa@mail.ru>
2023-04-08 00:46:26 +0300

    Small changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj.js';
 
 const map = new Map({
   target: 'map',
@@ -11,7 +12,7 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([23.650737, 52.101781]),
+    zoom: 14
   })
 });
