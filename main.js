1d2d461a8e286eae7b7b2c8a5c00764d4762fbb0
Pavel Merkushevich <11mpa@mail.ru>
2023-04-25 23:43:32 +0300

    Small changes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj.js';
 
@@ -9,7 +12,13 @@ const map = new Map({
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: 'quartals.json'
+      }),
+    }),
   ],
   view: new View({
     center: fromLonLat([23.650737, 52.101781]),
