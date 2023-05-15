c4a6381c9889a72fa6e9e9b09ede314fdf45446a
Pavel Merkushevich <11mpa@mail.ru>
2023-05-15 21:15:52 +0300

    Alpha version

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+import { Control } from 'ol/control.js';
 import './style.css';
 import {Map, View} from 'ol';
+import Style from 'ol/style/Style.js';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
@@ -23,5 +25,30 @@ const map = new Map({
   view: new View({
     center: fromLonLat([23.650737, 52.101781]),
     zoom: 14
+  })
+});
+
+var style = new Style({
+  fillColor: "${getColor}",
+  strokeColor: "#000000",
+  strokeWidth: 2,
+  fillOpacity: 0.5
+}, {
+  context: {
+    getColor: function (feature) {
+      var count = feature.attributes.count;
+      var color = "rgb(" + Math.round(255 - count * 10) + ",0," + Math.round(count * 10) + ")";
+      return color;
+    }
+  }
+});
+
+var layer = new VectorLayer( {
+  styleMap: new Style({
+    "default": style,
+    "select": {
+      fillColor: "#8aeeef",
+      strokeColor: "#32a8a9"
+    }
   })
 });
